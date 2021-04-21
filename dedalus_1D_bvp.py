bcb824e09ce2a689d78a25472193a564d1821a9b
GreyAudron <mschee@physics.utoronto.ca>
2021-04-21 16:14:44 -0400

    Finally got a decent plot

--- dedalus_1D_bvp.py
+++ dedalus_1D_bvp.py
@@ -6,7 +6,7 @@ This is a 1D script and should be ran serially.  It should converge within
 roughly a dozen iterations, and should take under a minute to run.
 
 The equation is of the general form:
-    dz(dz(P)) + P = 0
+    dx(dx(P)) + P = 0
     P(z=0) = 0
     P(z=L) = P_0
 where C^2 is some coefficient, and the equation is solved over the interval
@@ -27,30 +27,64 @@ logger = logging.getLogger(__name__)
 
 
 # Parameters
-z_min = 0
-z_max = np.pi/2.0
-Nz = 128
+x_min = 0
+L = 1
+x_max = L
+Nx = 32
 ncc_cutoff = 1e-10
 tolerance = 1e-5 #1e-10
 
+E = 1
+V_0 = 1.5*E
+
 # Build domain
-z_basis = de.Chebyshev('z', Nz, interval=(z_min, z_max), dealias=1)#2)
-domain = de.Domain([z_basis], np.float64)
+xbasis = de.Chebyshev('x', Nx, interval=(x_min, x_max))
+domain = de.Domain([xbasis], np.float64)#np.complex128)
 
 # Setup problem
-problem = de.NLBVP(domain, variables=['P', 'Pz'], ncc_cutoff=ncc_cutoff)
-problem.add_equation("dz(Pz) = -P")#, tau=False)
-problem.add_equation("Pz - dz(P) = 0")
-problem.add_bc("left(P) = 0")
-problem.add_bc("right(P) = 2")
+problem = de.NLBVP(domain, variables=['P1', 'P1x', 'P2', 'P2x', 'P3', 'P3x'], ncc_cutoff=ncc_cutoff)
+# Problem parameters
+problem.parameters['V_0'] = V_0
+problem.parameters['E'] = E
+
+# Section 1
+problem.add_equation("dx(P1x) = -E*P1")         # Schrodinger equation for V_0=0
+problem.add_equation("P1x - dx(P1) = 0")        # defining first derivative
+# Section 2
+problem.add_equation("dx(P2x) = (V_0-E)*P2")    # Schrodinger equation
+problem.add_equation("P2x - dx(P2) = 0")        # defining first derivative
+# Section 3
+problem.add_equation("dx(P3x) = -E*P3")         # Schrodinger equation for V_0=0
+problem.add_equation("P3x - dx(P3) = 0")        # defining first derivative
+# BCs: initial conditions at x=0
+problem.add_bc("left(P1) = 1")
+problem.add_bc("left(P1x) = 0")
+# BCs: continuous across section 1 to section 2
+problem.add_bc("right(P1) - left(P2) = 0")
+problem.add_bc("right(P1x) - left(P2x) = 0")
+# BCs: continuous across section 2 to section 3
+problem.add_bc("right(P2) - left(P3) = 0")
+problem.add_bc("right(P2x) - left(P3x) = 0")
 
 # Setup initial guess
 solver = problem.build_solver()
-z = domain.grid(0)
-P = solver.state['P']
-Pz = solver.state['Pz']
-P['g'] = np.cos(z)
-P.differentiate('z', out=Pz)
+x = domain.grid(0)
+
+# Section 1
+P1 = solver.state['P1']
+P1x = solver.state['P1x']
+P1['g'] = np.cos(100*x)
+P1.differentiate('x', out=P1x)
+# Section 2
+P2 = solver.state['P2']
+P2x = solver.state['P2x']
+P2['g'] = np.cos(100*x)#0*x+1
+P2.differentiate('x', out=P2x)
+# Section 2
+P3 = solver.state['P3']
+P3x = solver.state['P3x']
+P3['g'] = np.cos(100*x)#0*x+1
+P3.differentiate('x', out=P3x)
 
 # Iterations
 pert = solver.perturbations.data
@@ -66,16 +100,23 @@ logger.info('Iterations: {}'.format(solver.iteration))
 logger.info('Run time: %.2f sec' %(end_time-start_time))
 
 # Plotting the output
-print('len(z) =', len(z))
-print('len(P) =', len(P['g']))
+# print('len(x) =', len(x))
+# print('len(P1) =', len(P1['g']))
 # What Dedalus solved for
-plt.plot(z, P['g'], label=r'$\psi$')
-plt.plot(z, Pz['g'], label=r'$\psi_z$')
+plt.plot(x, P1['g'].real, label=r'$\psi_1$')
+plt.plot(x+L, P2['g'].real, label=r'$\psi_2$')
+plt.plot(x+2*L, P2['g'].real, label=r'$\psi_3$')
+plt.plot(x, P1x['g'], label=r'$\partial_x\psi_1$')
+plt.plot(x+L, P1x['g'], label=r'$\partial_x\psi_2$')
+plt.plot(x+2*L, P1x['g'], label=r'$\partial_x\psi_3$')
+# plt.plot(x, x_masks[0], label=r'$x_1$')
+# plt.plot(x, x_masks[1], label=r'$x_2$')
+# plt.plot(x, x_masks[2], label=r'$x_3$')
 # Analytical solutions
-plt.plot(z, 2*np.sin(z), '-.', label=r'$\psi_{ana}$')
-plt.plot(z, 2*np.cos(z), '-.', label=r'$\psi_{z, ana}$')
+# plt.plot(x, 2*np.sin(x), '-.', label=r'$\psi_{ana}$')
+# plt.plot(x, 2*np.cos(x), '-.', label=r'$\psi_{z, ana}$')
 
 plt.legend()
-plt.xlabel('z')
+plt.xlabel('x')
 plt.ylabel('Amplitude')
 plt.show()
--- rectangular_barrier.py
+++ rectangular_barrier.py
@@ -40,8 +40,47 @@ plt.style.use('dark_background')
 
 x = np.linspace(-1,1,100)
 
+E = 20
+V_0 = 1.3*E
+
+# Domain
+L = 0.5       # barrier width
+x_0 = 0
+flank = 2*L
+nx = 50
+x1 = np.linspace(-flank, 0, 2*nx)
+x2 = np.linspace(0, L, nx)
+x3 = np.linspace(L, flank+L, 2*nx)
+
+k = E
+l = V_0 - E
+
+# Coefficients
+F = (2j*k*l*np.exp(-1j*k*L))/((k**2-l**2)*np.sinh(l*L) + 2j*k*l*np.cosh(l*L))
+C = -(1j*k-l)*F*np.exp(1j*k*L + l*L)/(2*l)
+D =  (1j*k+l)*F*np.exp(1j*k*L - l*L)/(2*l)
+B = C + D - 1
+
+def psi_1(x):
+    return 1*np.exp(1j*k*x) + B*np.exp(-1j*k*x)
+def psi_2(x):
+    return C*np.exp(-l*x) + D*np.exp(l*x)
+def psi_3(x):
+    return F*np.exp(1j*k*x)
+
+print("BC1-2:", psi_1(0)-psi_2(0))
+print("BC2-3:", psi_2(L)-psi_3(L))
+
+psi1=psi_1(x1)
+psi2=psi_2(x2)
+psi3=psi_3(x3)
+
+def sq_amp(psi):
+    # return (psi*np.conj(psi)).real
+    return psi.real
 
 fig,ax = plt.subplots()
-ax.plot(x,P[:,0],'b-')
-ax.plot(x,V(x),'r--')
+plt.plot(x1, sq_amp(psi1), label=r'$\psi_1$')
+plt.plot(x2, sq_amp(psi2), label=r'$\psi_2$')
+plt.plot(x3, sq_amp(psi3), label=r'$\psi_3$')
 plt.show()
